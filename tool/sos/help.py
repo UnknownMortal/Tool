afa84885012c9dd8d0de15c6f1b8ef2b18212128
Unknown Mortal <104091630+UnknownMortal@users.noreply.github.com>
2022-06-20 12:54:55 +0530

    Update help.py

--- tool/sos/help.py
+++ tool/sos/help.py
@@ -4,7 +4,7 @@ from prettytable import PrettyTable
 from pyrogram import filters, Client
 from pyrogram.types import Message
 
-from tool.main import CMD_HELP
+from main import CMD_HELP
 from tool.sos.utility import split_list
 
 heading = "──「 **{0}** 」──\n"

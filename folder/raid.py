f562ab9c3b87f551160c80020c9e7f74c73d1d46
Unknown Mortal <104091630+UnknownMortal@users.noreply.github.com>
2022-06-20 17:26:14 +0530

    Create raid.py

---
+++ folder/raid.py
@@ -0,0 +1,23 @@
+from folder import dbb
+
+Rbun = dbb["RBAN"]
+
+
+async def rtool(user, reason="#MATHERCHOD"):
+    await Rbun.insert_one({"user": user, "reason": reason})
+
+
+async def runtool(user):
+    await Rbun.delete_one({"user": user})
+
+
+async def rban_list():
+    return [lo async for lo in Rbun.find({})]
+
+
+async def toolub_info(user):
+    kk = await Rbun.find_one({"user": user})
+    if not kk:
+        return False
+    else:
+        return kk["reason"]

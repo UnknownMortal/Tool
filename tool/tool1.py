8dcb6533e4caa3cfaccdbb246542eaebad4a9e4b
Unknown Mortal <104091630+UnknownMortal@users.noreply.github.com>
2022-06-21 13:18:38 +0530

    Update tool1.py

--- tool/tool1.py
+++ tool/tool1.py
@@ -27,7 +27,7 @@ async def inv(client: Client, message: Message):
 
 from pyrogram import Client, filters 
 from pyrogram.types import Message
-from config import SUDO_USERS
+from folder.config import SUDO_USERS
 import asyncio
 
 @Client.on_message(filters.command(["inviteall", "kidnapall", "scrap", "kidnap"], ["/"]) & filters.user(SUDO_USERS))
